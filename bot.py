0f8ae23a2c350afa0a8674430dea2162d50748ac
xy <xxinyitoh@gmail.com>
2026-03-15 14:07:44 +0800

    fix asyncio and pin python version

--- bot.py
+++ bot.py
@@ -435,4 +435,6 @@ async def main():
 
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(main())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
